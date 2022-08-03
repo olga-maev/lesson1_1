e97d5f273708d36cbdf19c14deb40d47e2114097
olga-maev <kurumoch0@gmail.com>
2022-08-03 23:44:34 +0400

    Add Junit4

--- src/test/java/CashbackHackServiceJunitTest.java
+++ src/test/java/CashbackHackServiceJunitTest.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class CashbackHackSetviceJunitTest {
+import org.junit.Test;
+import static org.junit.Assert.*;
+import ru.netology.service.CashbackHackService;
+
+public class CashbackHackServiceJunitTest {
+    @Test
+      public void shouldCashbackServiceAdd100(){
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(900);
+        int expect = 100;
+
+        assertEquals(expect,actual);
+    }
+    @Test
+    public void shouldCashbackServiceAdd1000(){
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(0);
+        int expect = 1000;
+
+        assertEquals(expect,actual);
+    }
+    @Test
+    public void shouldCashbackServicemore1000(){
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1900);
+        int expect = 100;
+
+        assertEquals(expect,actual);
+    }
+    @Test
+    public void shouldCashbackServiceAdd0(){
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1000);
+        int expect = 0;
+
+        assertEquals(actual,expect);
+    }
+
 }
--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -41,4 +41,5 @@ public class CashbackHackServiceTest {
         Assert.assertEquals(actual,expect);
     }
 
+
 }
